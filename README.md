afa79265dd0ac7feeb2fcadb4895505c2bad4bed
Andrew Cutler <spyroninja@gmail.com>
2019-01-16 16:26:42 -0500

    first commit

--- README.md
+++ README.md
@@ -66,3 +66,4 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+# Five Day Forecast -- using OpenWeatherMap API and React
--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
+// url with API key: http://api.openweathermap.org/data/2.5/forecast?q=10309,us&units=imperial&APPID=f9c997b1dda662b8a55159163b5a1342
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import DayCard from './DayCard';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      weather: [],
+      isLoaded: false
+    }
+  }
+  
+  componentDidMount() {
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=10309,us&units=imperial&APPID=f9c997b1dda662b8a55159163b5a1342')
+      .then(res => res.json())
+      .then(data => this.setState({
+        weather: data,
+        isLoaded: true
+      }))
+  }
+
   render() {
+    const display = this.state.isLoaded ? <DayCard weather={this.state.weather}/> : "Loading..."
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {display}
       </div>
     );
   }
 }
 
 export default App;
+
+
+//TODO: use map to populate 5 next days, and pass index (incremented by 8) as a prop

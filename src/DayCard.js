75d770be0c614b623a4d47ec81106eabcf0ce51c
Andrew Cutler <andrewkcutler@gmail.com>
2019-01-18 17:28:26 -0500

    improved retrieval of date info w Date, styling changes

--- src/DayCard.js
+++ src/DayCard.js
@@ -7,22 +7,35 @@ class DayCard extends Component {
   }
   state = {  }
   render() { 
-    // const dayOfWeek = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-    // const month = ["January","February","March","April","May","June","July","August","Septemer","October","November","December"]
-    // const day = new Date();
+    //calculate day of week, month name, and year from API data
+    const dayData = new Date(this.props.weather[this.props.index].dt_txt)
+
+    const dayOfWeek = ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+    const day = dayOfWeek[dayData.getDay()]
+
+    const monthOfYear = ["January","February","March","April","May","June","July","August","Septemer","October","November","December"]
+    const month = monthOfYear[dayData.getMonth()]
+
+    const year = dayData.getFullYear()
+
+    //source for weather icons
     const iconUrl = "http://openweathermap.org/img/w/"
+
     return ( 
       <div>
         <div className="card">
+          {/* SHOW DATE INFO */}
           <div className="day">
-            <p>{this.props.weather[this.props.index].dt_txt}</p>
-            <p></p>
+            <p>{day},</p>
+            <p>{month} {year}</p>
           </div>
 
+          {/* SHOW WEATHER ICON */}
           <div className="icon">
             <img src={[iconUrl] + this.props.weather[this.props.index].weather[0].icon + ".png"}/>
           </div>
 
+          {/* SHOW WEATHER DATA */}
           <div className="bottom">
             <div className="temp">
               <p>Temperature:</p>
